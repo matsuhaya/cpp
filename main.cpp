f4ecfa70f4907806975ca5100df0afabddac6929
matsuhaya <matsuhaya007@gmail.com>
2023-08-27 17:42:06 +0900

    add rep2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 template <typename T>
 inline void print(const vector<T>& v, string s = " ") {
   rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");
