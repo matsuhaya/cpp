5dbf18a251a51f15aaada2e926fa318a2134225b
matsuhaya <matsuhaya007@gmail.com>
2025-04-01 23:46:45 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,22 @@ template <typename T>
 inline void print(const vector<T>& v, string s = " ") {
   rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");
 }
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
 ll div_ceil(ll a, ll b) { return (a >= 0 ? (a + b - 1) : a) / b; }
 vector<int> di = {-1, 0, 0, 1}, dj = {0, -1, 1, 0};
 
